d1dea197fcbfb7f22a004d70d59f7a287cccc208
Bradford Hill <Brad.Hill@acstechnologies.com>
2012-01-17 13:47:50 -0500

    added the selectelement operators to the selectlistSe

--- SeleniumHelperClasses/SeleniumHelperClasses/ElementTypes/SelectListSe.cs
+++ SeleniumHelperClasses/SeleniumHelperClasses/ElementTypes/SelectListSe.cs
@@ -4,7 +4,9 @@
  Alt Email: Assiance@aol.com
  ********************************************************/
 using System;
+using System.Collections.Generic;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumHelperClasses.ElementTypes
 {
@@ -13,21 +15,47 @@ namespace SeleniumHelperClasses.ElementTypes
         public SelectListSe(IWebDriver webDriver, By by)
             : base(webDriver, by)
         {
+            SelectElement = new SelectElement(WebElement);
         }
 
         public SelectListSe(IWebElement webElement, By by)
             : base(webElement, by)
         {
+            SelectElement = new SelectElement(WebElement);
         }
 
         public SelectListSe(IWebDriver webDriver, By by, Func<IWebElement, bool> predicate)
             : base(webDriver, by, predicate)
         {
+            SelectElement = new SelectElement(WebElement);
         }
 
         public SelectListSe(IWebElement webElement, By by, Func<IWebElement, bool> predicate)
             : base(webElement, by, predicate)
         {
+            SelectElement = new SelectElement(WebElement);
+        }
+
+        private SelectElement SelectElement { get; set; }
+
+        public IList<IWebElement> AllSelectedOptions
+        {
+            get { return SelectElement.AllSelectedOptions; }
+        }
+
+        public bool IsMultiple
+        {
+            get { return SelectElement.IsMultiple; }
+        }
+
+        public IList<IWebElement> Options
+        {
+            get { return SelectElement.Options; }
+        }
+
+        public IWebElement SelectedOption
+        {
+            get { return SelectElement.SelectedOption; }
         }
 
         public string GetSelectedItem()
@@ -35,6 +63,41 @@ namespace SeleniumHelperClasses.ElementTypes
             string targetValue = WebElement.GetAttribute("value");
 
             return WebElement.FindElement(By.CssSelector("select [value='" + targetValue + "']")).Text;
+        }
+
+        public void DeselectAll()
+        {
+            SelectElement.DeselectAll();
+        }
+
+        public void DeselectByIndex(int index)
+        {
+            SelectElement.DeselectByIndex(index);
+        }
+
+        public void DeselectByText(string text)
+        {
+            SelectElement.DeselectByText(text);
+        }
+
+        public void DeselectByValue(string value)
+        {
+            SelectElement.DeselectByValue(value);
+        }
+
+        public void SelectByIndex(int index)
+        {
+            SelectElement.SelectByIndex(index);
+        }
+
+        public void SelectByText(string text)
+        {
+            SelectElement.SelectByText(text);
+        }
+
+        public void SelectByValue(string value)
+        {
+            SelectElement.SelectByValue(value);
         }
     }
 }
